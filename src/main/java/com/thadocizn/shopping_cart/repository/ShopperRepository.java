574adde1d9d04d9a3ea4f62ea09a94dd494d315a
ThaDocIzN <rodan72@msn.com>
2019-03-04 12:43:01 -0600

    added shopper repo

---
+++ src/main/java/com/thadocizn/shopping_cart/repository/ShopperRepository.java
@@ -0,0 +1,7 @@
+package com.thadocizn.shopping_cart.repository;
+
+import com.thadocizn.shopping_cart.model.Shopper;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopperRepository extends JpaRepository<Shopper, Long> {
+}
