fe05a0c184d5feb4a8ec8587a24227b23e217f36
ThaDocIzN <rodan72@msn.com>
2019-03-04 12:41:22 -0600

    added shopper class

---
+++ src/main/java/com/thadocizn/shopping_cart/model/Shopper.java
@@ -0,0 +1,24 @@
+package com.thadocizn.shopping_cart.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "customer")
+public class Shopper {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String billAddress;
+    private String shipAddress;
+    private String phoneNumber;
+    private String paymentMethod;
+
+    public Shopper() {
+    }
+
+}
