b81de221911ce98e1941f710179af6212e115d3b
ThaDocIzN <rodan72@msn.com>
2019-03-05 07:34:04 -0600

    updated order controller

--- src/main/java/com/thadocizn/shopping_cart/controller/OrderController.java
+++ src/main/java/com/thadocizn/shopping_cart/controller/OrderController.java
@@ -5,10 +5,12 @@ import com.thadocizn.shopping_cart.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/orders", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -20,5 +22,11 @@ public class OrderController {
     @GetMapping(value = "")
     public List<Order> listAllOrders(){
         return orderRepo.findAll();
+    }
+
+    // todo come back and update
+    @GetMapping(value = "/orders/id/{orderId}")
+    public Optional<Order> getProductsByName(@PathVariable long orderId){
+        return orderRepo.findById(orderId);
     }
 }
